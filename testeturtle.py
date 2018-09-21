94ca8a940023072a96f7399516afb85c7ba59642
NaomiFernanda <naomi.nfm@gmail.com>
2018-09-21 20:54:15 -0300

    teste com turtle comandos básicos

---
+++ testeturtle.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+import turtle
+#Turtle é um módulo em python para fazer desenhos na tela 
+
+pen = turtle.Pen()  #Constrói a caneta
+pen.forward(100)    #anda 100 posições para frente
+pen.right(45)       #gira o ponteiro em 45 graus para a direita
+pen.forward(100)    #anda mais 100 posições para frente
+
+# para trás caneta.backward(graus)
+#para esquerda caneta.left(graus)
+
+turtle.done() #mantém a aplicação aberta
